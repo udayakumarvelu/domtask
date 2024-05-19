bb9810bf7f3d207c4a5e51bccba985e981dc9ef0
udayakumarvelu <54550373+udayakumarvelu@users.noreply.github.com>
2024-05-19 18:56:14 +0530

    script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+function foo(){
+    var firstname = document.getElementById("firstname").value;
+    var MiddleName = document.getElementById("MiddleName").value;
+    var LastName = document.getElementById("LastName").value;
+    var email = document.getElementById("email").value;
+    var PhoneNumber = document.getElementById("PhoneNumber").value;
+
+    console.log(firstname);
+    console.log(MiddleName);
+    console.log(LastName);
+    console.log(email);
+    console.log(PhoneNumber);
+
+}
